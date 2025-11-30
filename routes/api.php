832b267ddcabc7a27713b5e68c0c17ad2714e6cc
Robin <robin1broos@gmail.com>
2025-11-30 22:12:32 +0100

    added versioning

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
-
+use App\Http\Controllers\Api\V1\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/status', function () {
+    return ["status" => "API is working!"];
+});
+
+
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('/posts', PostController::class);
+});
