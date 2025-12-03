be9374f7158a8f946abe443194da8a341160deda
Robin <robin1broos@gmail.com>
2025-12-03 15:48:35 +0100

    form request added

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\BlogPosts;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -12,7 +13,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        return BlogPosts::all();
     }
 
     /**
@@ -20,15 +21,22 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data= $request -> validate([
+            'title' => '|required|string',
+            'content' => 'sometimes|required|string',
+        ]);   
+        $data["author_id"] = 1 ;
+       $blogPost = BlogPosts::create($data);
+        
+        return response()-> json($blogPost, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Blogposts $post)
     {
-        //
+        return response()->json($post);
     }
 
     /**
@@ -36,14 +44,18 @@ class PostController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request -> validate([
+            'title' => '|required|string',
+            'content' => 'sometimes|required|string',
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(BlogPosts $post)
     {
-        //
+        $post->delete();
+        return response()->noContent();
     }
 }
