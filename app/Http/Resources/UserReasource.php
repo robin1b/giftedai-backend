34a4c52b3a796b4f41aa4898d1ebb129253ac6aa
Robin <robin1broos@gmail.com>
2025-12-03 21:09:24 +0100

    new author resource added

--- app/Http/Resources/UserReasource.php
+++ app/Http/Resources/UserReasource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class PostResource extends JsonResource
+class UserReasource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -16,11 +16,8 @@ class PostResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'title' => $this->title,
-            'content' => $this->content,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-            'author' => new UserReasource($this->author),
+            'name' => $this->name,
+            'email' => $this->email,
         ];
     }
 }
