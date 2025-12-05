34e388b254bc3c9cd90877481ab81ebb32491aca
Robin <robin1broos@gmail.com>
2025-12-05 01:27:38 +0100

    database test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use Illuminate\Container\Attributes\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return ['Laravel, env' => app()->version()];
+});
+Route::get('/dbcheck', function () {
+    return DB::getDatabaseName();
 });
